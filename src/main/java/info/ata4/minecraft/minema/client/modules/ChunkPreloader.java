4268035d31c4d6b190a2287c03b62b9e2da8eb09
Szperak <maciej.98@op.pl>
2015-01-18 17:41:55 +0100

    Preloading staff

--- src/main/java/info/ata4/minecraft/minema/client/modules/ChunkPreloader.java
+++ src/main/java/info/ata4/minecraft/minema/client/modules/ChunkPreloader.java
@@ -9,8 +9,14 @@
  */
 package info.ata4.minecraft.minema.client.modules;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.List;
+
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
@@ -19,7 +25,16 @@ import info.ata4.minecraft.minema.util.reflection.PrivateFields;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.ViewFrustum;
 import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.chunk.ChunkRenderDispatcher;
+import net.minecraft.client.renderer.chunk.RenderChunk;
+import net.minecraft.client.renderer.culling.ClippingHelper;
+import net.minecraft.client.renderer.culling.ClippingHelperImpl;
+import net.minecraft.client.renderer.culling.Frustum;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 
 /**
  *
@@ -36,12 +51,66 @@ public class ChunkPreloader extends CaptureModule {
     }
     
     @SubscribeEvent
-    public void onTick(RenderTickEvent evt) {
-        if (evt.phase != Phase.START) {
+    public void onTick(ClientTickEvent evt) {
+        if (evt.phase != Phase.END) {
             return;
         }
+        //MC.renderGlobal.
+        //Tessellator.getInstance().getWorldRenderer().startDrawing(7);
+        ChunkRenderDispatcher d =  ReflectionHelper.getPrivateValue(RenderGlobal.class, MC.renderGlobal,"field_174995_M");
+        ViewFrustum vf = ReflectionHelper.getPrivateValue(RenderGlobal.class, MC.renderGlobal,"field_175008_n");
+        //Method m = ReflectionHelper.findMethod(ViewFrustum.class, vf, new String[]{"func_178161_a"}, BlockPos.class);
+        /*int j = 248;
+        for(int k = -MC.gameSettings.renderDistanceChunks; k <= MC.gameSettings.renderDistanceChunks; k++)
+        {
+            for(int l = -MC.gameSettings.renderDistanceChunks; l <= MC.gameSettings.renderDistanceChunks; l++)
+            {
+            	
+            	
+            	RenderChunk renderchunk1;
+				try {
+					renderchunk1 = (RenderChunk) m.invoke(vf,new BlockPos((k << 4) + 8, j, (l << 4) + 8));
+				} catch (IllegalAccessException e) {
+
+					e.printStackTrace();
+					return;
+				} catch (IllegalArgumentException e) {
+					e.printStackTrace();
+					return;
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+					return;
+				}
+                //RenderChunk renderchunk1 = vf.func_178161_a(new BlockPos((k << 4) + 8, j, (l << 4) + 8));
+                if(renderchunk1 != null)
+                {
+                	//renderchunk1.func_178575_a(true);
+                    d.func_178505_b(renderchunk1);
+                    renderchunk1.func_178575_a(false);
+                }
+            }
+
+        }*/
         
-        //Tessellator.getInstance().getWorldRenderer().startDrawing(p_startDrawing_1_);
+        /*Frustum var8 = new Frustum(ClippingHelperImpl.getInstance());
+        Entity var9 = MC.getRenderViewEntity();
+        double var10 = var9.lastTickPosX + (var9.posX - var9.lastTickPosX) * (double)evt.renderTickTime;
+        double var12 = var9.lastTickPosY + (var9.posY - var9.lastTickPosY) * (double)evt.renderTickTime;
+        double var14 = var9.lastTickPosZ + (var9.posZ - var9.lastTickPosZ) * (double)evt.renderTickTime;
+        var8.setPosition(var10, var12, var14);*/
+        
+        int j = vf.field_178164_f.length;
+        for(int i = 0; i<j; i++){
+        	RenderChunk c = vf.field_178164_f[i];
+        	if(c != null && c.func_178569_m()) //var8.isBoundingBoxInFrustum(c.field_178591_c))
+            {
+            	//renderchunk1.func_178575_a(true);
+                d.func_178505_b(c);
+                c.func_178575_a(false);
+            }
+        }
+        
+        //MC.renderGlobal.func_174967_a(System.nanoTime()+900000000L);
     }
 
     @Override
